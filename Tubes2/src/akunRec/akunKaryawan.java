25fb04adabbe8ee3e3d6deacbdb32b27c2a063a9
Kelfin Antonius <91953273+KelfinTDetonator@users.noreply.github.com>
2022-12-11 19:14:08 +0700

    Add files via upload

---
+++ Tubes2/src/akunRec/akunKaryawan.java
@@ -0,0 +1,21 @@
+package akunRec;
+
+public class akunKaryawan {
+    private int IDkaryawan;
+    private int passwordKaryawan;
+    private String namaKaryawan;
+
+    public akunKaryawan(int ID, int pw, String nama){
+        this.IDkaryawan = ID;
+        this.passwordKaryawan = pw;
+        this.namaKaryawan = nama;
+    }
+
+    public boolean validasiPass(int pwKary){
+        if(pwKary == passwordKaryawan) {
+            return true;
+        }
+         else {return false;}
+    }
+    public int getAkunID(){return IDkaryawan;}
+}
---
+++ Tubes2/src/akunRec/dbResepsionis.java
@@ -0,0 +1,28 @@
+package akunRec;
+
+public class dbResepsionis {
+    private akunKaryawan[] akun;
+
+    public dbResepsionis(){
+        akun = new akunKaryawan[2];
+        akun[0] = new akunKaryawan(121, 222, "Fatmawati");
+        akun[1] = new akunKaryawan(122, 101122, "Danang Kurniawan");
+    }
+    private akunKaryawan getAkun(int IDkaryawan){
+        for (akunKaryawan activeAkun : akun){
+            if(activeAkun.getAkunID() == IDkaryawan){
+                return activeAkun;
+            }
+        }
+        return null; //Jika akun tidak ada,return null
+    }
+    public boolean autentikasiAkun(int IDkaryawan, int pw) {
+        akunKaryawan karyawan = getAkun(IDkaryawan);
+
+        if (karyawan != null) {
+            return karyawan.validasiPass(pw);
+        } else {
+            return false;
+        }
+    }
+}
